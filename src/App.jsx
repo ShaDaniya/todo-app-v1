b5627aa0ab60edfcfd35fd55d85e3c483ff2182f
Daniya <daniyashagieva@gmail.com>
2022-12-17 16:56:52 +0300

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
+import './App.css'
+
+
+
+function App() {
+  const [todos, setTodos] = useState(['one', 'two', 'three'])
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm />
+      <TodoList todos={todos}/>
+    </div>
+  );
+}
+
+export default App;
